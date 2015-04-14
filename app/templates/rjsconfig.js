644287911a8b85b8ba66885c20096fb031a5fcea
Nicholas Antonov <nicholasantonov@gmail.com>
2015-04-14 16:04:49 -0400

    seperate mvvm generator from main generator

--- app/templates/rjsconfig.js
+++ app/templates/rjsconfig.js
@@ -1,21 +1,11 @@
 /*jshint ignore:start*/
 requirejs({
-  baseUrl: 'src',
-  scalejs: {
-    extensions: [ //TODO: FIX THIS TEMPORARY HACK, this map and extensions assume mvvm pattern
-      'scalejs.mvvm',
-      'scalejs.statechart-scion'
-    ]
-  },
-  map: {
-    '*': {
-      sandbox: 'scalejs.sandbox',
-      bindings: 'scalejs.mvvm.bindings',
-      views: 'scalejs.mvvm.views'
+    'baseUrl': 'src',
+    'scalejs': {
+        'extensions': [
+        ]
+      },
+    'paths': {
     }
-  },
-  paths: {
-  }
 });
 /*jshint ignore:end*/
-
--- mvvmmodule/index.js
+++ mvvmmodule/index.js
@@ -23,33 +23,84 @@ module.exports = yeoman.generators.Base.extend({
         }.bind(this));
     },
 
-    writing: function () {
-        var modulePath = 'src/app/' + this.name + '/',
-            context = { name: this.name };
+    writing: {
+        makeModule: function () {
+            var modulePath = 'src/app/' + this.name + '/',
+                context = { name: this.name };
 
-        this.fs.copyTpl(
-            this.templatePath('module.js'),
-            this.destinationPath(modulePath + this.name + 'Module.js'),
-            context
-        );
+            this.fs.copyTpl(
+                this.templatePath('module.js'),
+                this.destinationPath(modulePath + this.name + 'Module.js'),
+                context
+            );
 
-        this.fs.copyTpl(
-            this.templatePath('bindings/bindings.js'),
-            this.destinationPath(modulePath + 'bindings/' + this.name + 'Bindings.js'),
-            context
-        );
+            this.fs.copyTpl(
+                this.templatePath('bindings/bindings.js'),
+                this.destinationPath(modulePath + 'bindings/' + this.name + 'Bindings.js'),
+                context
+            );
 
-        this.fs.copyTpl(
-            this.templatePath('viewmodels/viewmodel.js'),
-            this.destinationPath(modulePath + 'viewmodels/' + this.name + 'ViewModel.js'),
-            context
-        );
+            this.fs.copyTpl(
+                this.templatePath('viewmodels/viewmodel.js'),
+                this.destinationPath(modulePath + 'viewmodels/' + this.name + 'ViewModel.js'),
+                context
+            );
 
-        this.fs.copyTpl(
-            this.templatePath('views/name.html'),
-            this.destinationPath(modulePath + 'views/' + this.name + '.html'),
-            context
-        );
+            this.fs.copyTpl(
+                this.templatePath('views/name.html'),
+                this.destinationPath(modulePath + 'views/' + this.name + '.html'),
+                context
+            );
+        },
+        updateConfig: function () {
+            //Add statechart and mvvm extensions and mappings to the rjs config
+            var fs = require('fs'),
+                _ = require('lodash'),
+                esprima = require('esprima'),
+                escodegen = require('escodegen'),
+                filePath = './rjsconfig.js',
+                additionalConfig = {
+                    scalejs: {
+                        extensions: [
+                            'scalejs.mvvm',
+                            'scalejs.statechart-scion'
+                        ]
+                    },
+                    map: {
+                        '*': {
+                            sandbox: 'scalejs.sandbox',
+                            bindings: 'scalejs.mvvm.bindings',
+                            views: 'scalejs.mvvm.views'
+                        }
+                    }
+                },
+                fileString,
+                parsedFile,
+                parsedConfig,
+                jsonifiedParsedConfig,
+                mergedConfig,
+                newConfig,
+                finalFile;
+
+            fileString = fs.readFileSync(filePath, "utf8");
+            parsedFile = esprima.parse(fileString);
+            parsedConfig = parsedFile.body[0].expression.arguments[0];
+            jsonifiedParsedConfig = eval('(' + escodegen.generate(parsedConfig) + ')');
+            mergedConfig = _.merge(additionalConfig, jsonifiedParsedConfig, function(a, b) {
+                if (_.isArray(a)) {
+                    return a.concat(b);
+                }
+            });
+            parsedFile.body[0].expression.arguments[0] = esprima.parse('(' + JSON.stringify(mergedConfig) + ')').body[0].expression
+            finalFile = escodegen.generate(parsedFile);
+            finalFile = '/*jshint ignore:start*/\n' + finalFile;
+            finalFile += '\n/*jshint ignore:end*/\n';
+            fs.writeFile(filePath, finalFile, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            });
+        }
 
     },
 
